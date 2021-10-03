27e0c822eabebfa0cec3e0902d48938d19ace101
Joe <joe.delia1993@gmail.com>
2021-10-03 13:34:18 -0400

    add action creators and implement on frontend (install redux-thunk for handling requests)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from './store/index'
 
 function App() {
+    // access account value from redux store
+    const account = useSelector((state) => state.account)
+    const dispatch = useDispatch()
+
+    const { deposit, withdraw } = bindActionCreators(actionCreators, dispatch)
+
     return (
         <div className="App">
-      
+            <h1>{account}</h1>
+            <button onClick={() => deposit(100)}>Deposit</button>
+            <button onClick={() => withdraw(100)}>Withdraw</button>
         </div>
     );
 }
